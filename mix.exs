67b391f0a9f349a360ef60d8db9c640dd456959c
Calle Englund <calle@discord.bofh.se>
2020-05-17 20:57:18 +0200

    Add some dev dependencies and project metadata

--- mix.exs
+++ mix.exs
@@ -7,22 +7,36 @@ defmodule Tagged.MixProject do
       version: "0.1.0",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      package: package(),
+      description: "Construct and destructure tagged value tuples",
+      test_coverage: [tool: ExCoveralls]
     ]
   end
 
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      # extra_applications: [:logger]
     ]
   end
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:dialyxir, "~> 0.5", only: :dev, runtime: false},
+      {:ex_doc, "~> 0.21", only: :dev, runtime: false},
+      {:excoveralls, "~> 0.3", only: :test, runtime: false}
+    ]
+  end
+
+  defp package do
+    [
+      licenses: ["MIT"],
+      maintainers: ["Calle Englund"],
+      links: %{
+        "GitHub" => "https://github.com/notCalle/elixir-tagged"
+      }
     ]
   end
 end
