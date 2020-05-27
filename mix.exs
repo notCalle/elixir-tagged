d81e20f500595a400e1ba3642809791ac9b37ceb
Calle Englund <calle@discord.bofh.se>
2020-05-27 23:50:55 +0200

    Bump version 0.4.0

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Tagged.MixProject do
   Generates definitions of various things related to `{:tag, value}` tuples.
   """
 
-  @version "0.3.0"
+  @version "0.4.0"
 
   @deps [
     {:keyword_validator, "~> 1.0"},
