ac7bc2b95f84c9eda68059b4db739ccae2db8702
Calle Englund <calle@discord.bofh.se>
2020-06-21 19:19:13 +0200

    Improve PipeWith test coverage

--- test/tagged_pipe_with_test.exs
+++ test/tagged_pipe_with_test.exs
@@ -4,23 +4,25 @@ defmodule TaggedPipeWithTest do
   defmodule PipeWith do
     use Tagged
 
-    deftagged not_an_integer
-    deftagged not_a_number
+    deftaggedp not_an_integer(number()), guard: false
+    deftaggedp not_a_number(), guard: false
 
     @type reason :: not_an_integer() | not_a_number()
 
     deftagged success(integer())
     deftagged failure(reason())
+    deftagged nil, as: nothing()
 
-    @type result :: success() | failure()
+    @type result :: success() | failure() | nothing()
 
+    def validate_input(nil), do: nothing()
     def validate_input(x) when is_integer(x), do: success(x)
     def validate_input(x), do: failure(not_an_integer(x))
 
     def next_number(x), do: x + 1
 
-    def try_recovery({_, x}) when is_number(x), do: success(floor(x))
-    def try_recovery({_, x}), do: failure(not_a_number(x))
+    def try_recovery(not_an_integer(x)) when is_number(x), do: success(floor(x))
+    def try_recovery(_), do: failure(not_a_number())
   end
 
   doctest Tagged.PipeWith
